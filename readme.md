935b1ca18b4d72fcad8316cffd667f615ea6b014
KiKoS <riadh.kikos123@hotmail.fr>
2020-07-22 03:41:30 +0100

    Add readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Ray tracer
+A rust attempt at a ray tracer.
+
+## Build
+For building you just need [rust](https://rustup.rs/) and run: 
+1. cargo build
+2. cargo run -- > img.ppm
+
+
+## Example
+![Examples](/images/gradient_result.png "Normal shaded sphere")
---
+++ src/math/ray.rs
@@ -0,0 +1,17 @@
+use super::Point3;
+use super::Vec3;
+
+pub struct Ray {
+    pub origin: Point3<f64>,
+    pub direction: Vec3<f64>,
+}
+
+impl Ray {
+    pub fn new(origin: Point3<f64>, direction: Vec3<f64>) -> Ray {
+        Ray { origin, direction }
+    }
+
+    pub fn at(&self,t: f64) -> Point3<f64>{
+        self.origin + self.direction * t
+    }
+}
